f9437adbdddcfa1ba769f6e5939eedcc0960affa
Josiah Elisha <caprielo101@gmail.com>
2019-07-09 11:29:17 +0700

    Import AudioKit

--- Anore Project/ViewController.swift
+++ Anore Project/ViewController.swift
@@ -7,14 +7,76 @@
 //
 
 import UIKit
+import AudioKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var amplitudeLabel: UILabel!
+    @IBOutlet weak var frequencyLabel: UILabel!
+    
+    @IBOutlet weak var noteNameWithFlatsLabel: UILabel!
+    @IBOutlet weak var noteNameWithSharpsLabel: UILabel!
+    var mic: AKMicrophone!
+    var tracker: AKFrequencyTracker!
+    var silence: AKBooster!
+    var timer: Timer!
+    
+    let noteFrequencies = [16.35, 17.32, 18.35, 19.45, 20.6, 21.83, 23.12, 24.5, 25.96, 27.5, 29.14, 30.87]
+    let noteNamesWithSharps = ["C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B"]
+    let noteNamesWithFlats = ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mic = AKMicrophone()
+        tracker = AKFrequencyTracker(mic)
+        silence = AKBooster(tracker, gain: 1)
+        AKSettings.audioInputEnabled = true
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        do {
+            try AudioKit.stop()
+            AudioKit.output = silence
+            try AudioKit.start()
+        } catch {
+            AKLog("error")
+        }
+        _ = Timer.scheduledTimer(timeInterval: 0.1,
+                                 target: self,
+                                 selector: #selector(updateUI),
+                                 userInfo: nil,
+                                 repeats: true)
+    }
+    
+    @objc func updateUI() {
+        if tracker.amplitude > 0.1 {
+            frequencyLabel.text = String(format: "%0.1f", tracker.frequency)
+            
+            var frequency = Float(tracker.frequency)
+            while (frequency > Float(noteFrequencies[noteFrequencies.count-1])) {
+                frequency = frequency / 2.0
+            }
+            while (frequency < Float(noteFrequencies[0])) {
+                frequency = frequency * 2.0
+            }
+            
+            var minDistance: Float = 10000.0
+            var index = 0
+            
+            for i in 0..<noteFrequencies.count {
+                let distance = fabsf(Float(noteFrequencies[i]) - frequency)
+                if (distance < minDistance){
+                    index = i
+                    minDistance = distance
+                }
+            }
+            let octave = Int(log2f(Float(tracker.frequency) / frequency))
+            noteNameWithSharpsLabel.text = "\(noteNamesWithSharps[index])\(octave)"
+            noteNameWithFlatsLabel.text = "\(noteNamesWithFlats[index])\(octave)"
+        }
+        amplitudeLabel.text = String(format: "%0.2f", tracker.amplitude)
+    }
 }
 
