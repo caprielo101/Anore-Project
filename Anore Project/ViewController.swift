2cbf1e8bd4999cb18337190cd89b4dc57547f04f
Josiah Elisha <caprielo101@gmail.com>
2019-07-09 11:25:47 +0700

    Initial Commit

---
+++ Anore Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Anore Project
+//
+//  Created by Josiah Elisha on 09/07/19.
+//  Copyright © 2019 Josiah Elisha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
